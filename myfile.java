c3e9c8b5350d2bbbbd5f69293c942e5e0d2b8ee8
koustubhjuvekar <koustubhjuvekarofficial@gmail.com>
2025-08-10 11:40:05 +0000

    mycommit2

--- myfile.java
+++ myfile.java
@@ -1,18 +1,18 @@
 public class myfile {
     public static void main(String[] args) {
-        System.out.println("The Rookie - TV Series Information");
-        System.out.println("----------------------------------");
-        System.out.println("Genre: Police procedural, Crime drama, Action");
-        System.out.println("Created by: Alexi Hawley");
-        System.out.println("First aired: October 16, 2018");
-        System.out.println("Network: ABC");
+        System.out.println("S.W.A.T. - TV Series Information");
+        System.out.println("--------------------------------");
+        System.out.println("Genre: Action, Crime drama, Police procedural");
+        System.out.println("Created by: Robert Hamner and developed by Rick Husky");
+        System.out.println("First aired: November 2, 2017 (CBS reboot series)");
         System.out.println("Main Cast:");
-        System.out.println("  - Nathan Fillion as John Nolan");
-        System.out.println("  - Alyssa Diaz as Angela Lopez");
-        System.out.println("  - Richard T. Jones as Wade Grey");
-        System.out.println("  - Melissa O'Neil as Lucy Chen");
-        System.out.println("  - Eric Winter as Tim Bradford");
-        System.out.println("Plot: Follows John Nolan, the oldest rookie in the LAPD,");
-        System.out.println("      as he navigates challenges in his career and personal life.");
+        System.out.println("  - Shemar Moore as Daniel 'Hondo' Harrelson");
+        System.out.println("  - Jay Harrington as David 'Deacon' Kay");
+        System.out.println("  - Alex Russell as Jim Street");
+        System.out.println("  - Lina Esco as Christina 'Chris' Alonso");
+        System.out.println("  - Kenny Johnson as Dominique Luca");
+        System.out.println("Plot: Centers on a S.W.A.T. sergeant, Hondo Harrelson,");
+        System.out.println("      who leads a highly-trained, elite unit in Los Angeles,");
+        System.out.println("      balancing loyalty to the streets and allegiance to the force.");
     }
 }
