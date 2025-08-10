6e1521238ffbe4ba59212cce091e52f62110737d
koustubhjuvekar <koustubhjuvekarofficial@gmail.com>
2025-08-10 09:01:34 +0000

    mycommit1

---
+++ myfile.java
@@ -0,0 +1,18 @@
+public class myfile {
+    public static void main(String[] args) {
+        System.out.println("The Rookie - TV Series Information");
+        System.out.println("----------------------------------");
+        System.out.println("Genre: Police procedural, Crime drama, Action");
+        System.out.println("Created by: Alexi Hawley");
+        System.out.println("First aired: October 16, 2018");
+        System.out.println("Network: ABC");
+        System.out.println("Main Cast:");
+        System.out.println("  - Nathan Fillion as John Nolan");
+        System.out.println("  - Alyssa Diaz as Angela Lopez");
+        System.out.println("  - Richard T. Jones as Wade Grey");
+        System.out.println("  - Melissa O'Neil as Lucy Chen");
+        System.out.println("  - Eric Winter as Tim Bradford");
+        System.out.println("Plot: Follows John Nolan, the oldest rookie in the LAPD,");
+        System.out.println("      as he navigates challenges in his career and personal life.");
+    }
+}
